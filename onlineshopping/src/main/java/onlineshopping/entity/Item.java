2ab014995bbaad1306a9ce8d74ae2fa727da2b73
Mdsoln <muddyfakih98@gmail.com>
2024-03-23 11:06:58 +0300

    item/product payload of the system

--- onlineshopping/src/main/java/onlineshopping/entity/Item.java
+++ onlineshopping/src/main/java/onlineshopping/entity/Item.java
@@ -1,4 +1,43 @@
 package onlineshopping.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "items")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Item {
+    @Id
+    @SequenceGenerator(
+            name = "item_sequence",
+            sequenceName = "item_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "item_sequence"
+    )
+    private Long itemId;
+
+
+    @Column(name = "item_name", nullable = false)
+    private String itemName;
+
+    @Column(name = "price", nullable = false)
+    private float actual_price;
+
+    @Column(name = "discount")
+    private float discount_price;
+
+    private int quantity;
+    private String description;
+
+    @Column(name = "images")
+    private String imageUrl;
 }
