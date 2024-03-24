2bc9da43ec19ac18135b1626ea0b60c370e529c8
Mdsoln <muddyfakih98@gmail.com>
2024-03-25 00:21:06 +0300

    security configuration

--- onlineshopping/src/main/java/onlineshopping/jwt/config/SecurityConfig.java
+++ onlineshopping/src/main/java/onlineshopping/jwt/config/SecurityConfig.java
@@ -1,4 +1,44 @@
 package onlineshopping.jwt.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthFilter jwtAuthenticationFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf)->csrf
+                        .ignoringRequestMatchers("/api/v1/base/**","/api/v1/user/cart/checkout","/api/v1/search/**")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/api/v1/base/**")
+                                .permitAll()
+                                .requestMatchers("/api/v1/user/cart/checkout","/payments/**")
+                                .permitAll()
+                                .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/entrepreneur").hasAnyRole("ENTREPRENEUR","ADMIN")
+                                .anyRequest()
+                                .authenticated()
+                ).sessionManagement(
+                        session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).authenticationProvider(authenticationProvider
+                ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
