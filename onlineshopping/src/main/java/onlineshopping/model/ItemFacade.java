fc5b1f193331c4387fe98d9569d347cb94ed1a98
Mdsoln <muddyfakih98@gmail.com>
2024-03-25 14:53:07 +0300

    item JSON payload

--- onlineshopping/src/main/java/onlineshopping/model/ItemFacade.java
+++ onlineshopping/src/main/java/onlineshopping/model/ItemFacade.java
@@ -1,4 +1,19 @@
 package onlineshopping.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class ItemFacade {
+    private String item_name;
+    private int stoke_quantity;
+    private float actual_price;
+    private float discount_price;
+    private String description;
+    private String imageUrl;
 }
