ecd0a350606ccbb455574fe6533f3da6dd25c31c
Mdsoln <muddyfakih98@gmail.com>
2024-03-26 01:15:36 +0300

    updating repo interface by adding object methods for querying

--- onlineshopping/src/main/java/onlineshopping/repo/ItemRepo.java
+++ onlineshopping/src/main/java/onlineshopping/repo/ItemRepo.java
@@ -2,10 +2,20 @@ package onlineshopping.repo;
 
 import onlineshopping.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepo extends JpaRepository<Item, Long> {
     Item findByItemNo(String itemNo);
+
+    @Query("SELECT DISTINCT i.itemName FROM Item i")
+    List<String> findAllItems();
+
+    List<Item> findByItemNameContainingIgnoreCase(String itemQuery);
+
+    Item findByItemNameIgnoreCase(String queryString);
 
 }
