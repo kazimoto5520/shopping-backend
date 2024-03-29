f69d12f30322d4d6c12d6ecf72b20073d4bad0c5
Mdsoln <muddyfakih98@gmail.com>
2024-03-29 15:19:58 +0300

    provider type response

--- onlineshopping/src/main/java/onlineshopping/model/PaymentResponse.java
+++ onlineshopping/src/main/java/onlineshopping/model/PaymentResponse.java
@@ -1,4 +1,17 @@
 package onlineshopping.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class PaymentResponse {
+    private boolean success;
+    private String message;
+    private String transactionalID;
+    private Map<String,String> additionalData;
 }
