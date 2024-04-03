7294923c950a5b3ba43e241166ffb12f7f6cd889
Mdsoln <muddyfakih98@gmail.com>
2024-04-03 11:49:34 +0300

    order test mocking class

--- onlineshopping/src/test/java/onlineshopping/repo/OrderRepoTest.java
+++ onlineshopping/src/test/java/onlineshopping/repo/OrderRepoTest.java
@@ -1,11 +1,10 @@
 package onlineshopping.repo;
 
-import onlineshopping.entity.CartItem;
+import onlineshopping.model.CartItem;
 import onlineshopping.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
